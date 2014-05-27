d15a06070bc59a67b8006e1ca7110a308d46768f
Artur Delura <adelura@gmail.com>
2014-05-27 15:25:00 +0200

    Created SDK builder in dev/builder.

---
+++ dev/builder/app.js
@@ -0,0 +1,152 @@
+var fs = require( 'fs' ),
+    ncp = require( 'ncp' ),
+    path = require( 'path' ),
+    call = require( 'when/node' ).call,
+    cheerio = require( 'cheerio' ),
+    when = require( 'when' ),
+    whenFs = require( 'when/node' ).liftAll( fs ),
+    whenKeys = require( 'when/keys' ),
+    _ = require( 'lodash-node' ),
+
+    Sample = require( './lib/Sample' ),
+
+    SAMPLES_PATH = '../../samples',
+    RELEASE_PATH = '../release',
+
+    validCategories = JSON.parse( fs.readFileSync( './categories.json', 'utf8' ) ).categories,
+    samples = [],
+    index = null,
+    categories = {};
+
+require( 'when/monitor/console' );
+
+// sync
+function selectFilesSync( filesArr ) {
+    return _.filter( filesArr, function( fileName ) {
+        return fileName.match( /.html$/i ) != null;
+    } );
+}
+
+// return promise
+function readFiles( filesArr ) {
+    var filesReadPromises = _.map( filesArr, function( fileName ) {
+        var promise = whenFs.readFile( SAMPLES_PATH + '/' + fileName, 'utf8' );
+
+        return [ fileName, promise ];
+    } );
+
+    filesReadPromises = _.object( filesReadPromises );
+
+    return whenKeys.all( filesReadPromises );
+}
+
+// return array of Sample instances
+function setupSamplesSync( _samples ) {
+    index = new Sample( 'index', _samples[ 'index.html' ] );
+
+    var removed = delete _samples[ 'index.html' ];
+
+    samples = _.map( _samples, function( fileContent, fileName ) {
+        return new Sample( fileName.split( '.' )[ 0 ], fileContent, index );
+    } );
+
+    if ( !removed )
+        throw 'Could not found "index.html" file in samples directory.';
+
+    return samples;
+}
+
+// return array of categories
+function parseCategoriesSync( samples ) {
+    console.log( 'Parsing files.' );
+
+    _.each( samples, function( sample ) {
+        var category, subcategory;
+
+        category = categories[ sample.category ] = categories[ sample.category ] || {
+            name: sample.category,
+            subcategories: {}
+        };
+
+        subcategory = category.subcategories[ sample.subcategory ] = category.subcategories[ sample.subcategory ] || {
+            name: sample.subcategory,
+            weight: null,
+            samples: []
+        };
+
+        var foundPredefinedCategory = _.find( validCategories, function( category ) {
+            return category.name == sample.category;
+        } );
+        if ( !foundPredefinedCategory )
+            throw 'Could not find predefined category "' + sample.category + '" in sample: ' + sample.name;
+
+        var foundPredefinedSubcategory = _.find( foundPredefinedCategory.subcategories, function( subcategory ) {
+            return subcategory == sample.subcategory;
+        } );
+        if ( !foundPredefinedSubcategory )
+            throw 'Could not find predefined subcategory "' + sample.subcategory + '" in sample: ' + sample.name;
+
+        subcategory.samples.push( sample );
+    } );
+
+    _.each( categories, function( category ) {
+        _.each( category.subcategories, function( subcategory ) {
+            subcategory.samples.sort( function( a, b ) {
+                return b.weight - a.weight;
+            } );
+        } );
+    } );
+
+    return categories;
+}
+
+// return promise
+function copyFiles() {
+    console.log( 'Copying files.' );
+    var options = {};
+
+    options.filter = function( name ) {
+        var basename = path.basename( name ),
+            startFromDot = !!basename.match( /^\./i ),
+            isReleaseDir = basename == 'release',
+            isSamplesDir = basename == 'samples';
+
+        return !( startFromDot || isReleaseDir || isSamplesDir );
+    };
+
+    return call( ncp, '../../', RELEASE_PATH, options );
+}
+
+// return promise
+function prepareSamplesDir() {
+    fs.mkdirSync( RELEASE_PATH + '/samples' );
+
+    return call( ncp, '../../samples/assets', RELEASE_PATH + '/samples/assets' );
+}
+
+// sync method
+function prepareSamplesFilesSync() {
+    _.each( samples, function( sample ) {
+        sample.setSidebar( categories );
+
+        fs.writeFileSync( RELEASE_PATH + '/samples/' + sample.name + '.html', sample.$.html(), 'utf8' );
+    } );
+
+    index.setSidebar( categories );
+    fs.writeFileSync( RELEASE_PATH + '/samples/index.html', index.$.html(), 'utf8' );
+}
+
+// return promise
+function readSamplesDir() {
+    console.log( 'Reading sample directory' );
+    return whenFs.readdir( SAMPLES_PATH );
+}
+
+copyFiles()
+    .then( readSamplesDir )
+    .then( selectFilesSync )
+    .then( readFiles )
+    .then( setupSamplesSync )
+    .then( parseCategoriesSync )
+    .then( prepareSamplesDir )
+    .then( prepareSamplesFilesSync );
---
+++ dev/builder/lib/Sample.js
@@ -0,0 +1,82 @@
+var cheerio = require( 'cheerio' ),
+    _ = require( 'lodash-node' ),
+    TITLE_PREFIX = 'CKEditor SDK &raquo; Samples &raquo; ';
+
+function Sample( name, content, index ) {
+    this.$ = cheerio.load( content );
+
+    this.name = name;
+
+    this.$title = this.$( 'title' );
+    this.title = this.$title.text();
+    this.$title.html( TITLE_PREFIX + this.title );
+
+    this.$header = this.$( 'header' );
+    this.$footer = this.$( 'footer' );
+
+    if ( index ) {
+        this.$header.html( index.$header.html() );
+        this.$footer.html( index.$footer.html() );
+    }
+
+    this.$nav = this.$( 'nav.sdk-sidebar' );
+
+    if ( this.name != 'index' ) {
+        this.category = this.$( 'meta[name="sdk-category"]' );
+        if ( this.category.length != 1 )
+            throw 'Invalid number of sdk-category meta tags in sample: ' + this.name;
+        this.category = this.category.attr( 'content' );
+
+        this.subcategory = this.$( 'meta[name="sdk-subcategory"]' );
+        if ( this.subcategory.length != 1 )
+            throw 'Invalid number of sdk-subcategory meta tags in sample: ' + this.name;
+        this.subcategory = this.subcategory.attr( 'content' );
+
+        this.weight = this.$( 'meta[name="sdk-weight"]' );
+        if ( this.weight.length > 1 )
+            throw 'Invalid number of sdk-weight meta tags in sample: ' + this.name;
+        this.weight = Number( this.weight.attr( 'content' ) ) || 1;
+    }
+}
+
+Sample.prototype = {
+    setSidebar: function( categories ) {
+        if ( this.name != 'index' )
+            this.$nav.html( Sample.createSidebar( categories, _.pick( this, 'category', 'subcategory', 'name' ) ) );
+        else
+            this.$nav.html( Sample.createSidebar( categories ) );
+    }
+};
+
+// return sidebar HTML string
+Sample.createSidebar = function( categories, highlight ) {
+    var result = [];
+
+    _.each( categories, function( category ) {
+        result.push( '<h2>' + category.name + '</h2>' );
+
+        _.each( category.subcategories, function( subcategory ) {
+            if ( !subcategory.samples.length )
+                return;
+
+            result.push( '<h3>' + subcategory.name + '</h3>' );
+
+            result.push('<ul>');
+            _.each( subcategory.samples, function( sample ) {
+                var highlightMe = (
+                    highlight &&
+                    sample.category == highlight.category &&
+                    sample.subcategory == highlight.subcategory &&
+                    sample.name == highlight.name
+                    );
+
+                result.push( '<li><a class="' + ( highlightMe ? 'active' : '' )  + '" href="' + sample.name + '.html">' + sample.title + '</a></li>' );
+            } );
+            result.push('</ul>')
+        } );
+    } );
+
+    return result.join( '' );
+};
+
+module.exports = Sample;
