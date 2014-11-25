614dd1fa5cd30e37caba09277891610899a899bc
Wiktor Walc <w.walc@cksource.com>
2014-11-25 17:07:08 +0100

    Updated CKEditor to 4.4.6

--- samples/assets/simplesample.js
+++ samples/assets/simplesample.js
@@ -167,7 +167,7 @@
 				}
 			}
 
-			headResources.unshift( '<script src="http://cdn.ckeditor.com/4.4.5/standard-all/ckeditor.js"></script>' );
+			headResources.unshift( '<script src="http://cdn.ckeditor.com/4.4.6/standard-all/ckeditor.js"></script>' );
 			headResources = headResources.join( '' );
 
 			function getTemplatePre( headResources, title ) {
