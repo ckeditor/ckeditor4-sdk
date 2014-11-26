3fed7d079714ee22dfbb405a6aa76501a0c68d3e
Artur Delura <adelura@gmail.com>
2014-11-26 12:47:05 +0100

    Tests: Written tests for download sample source code.

--- tests/one.js
+++ tests/one.js
@@ -1,10 +1,27 @@
 ( function() {
+	var predefinedText = '<p>This is some <strong>sample text</strong>. You are using <a href="http://ckeditor.com/">CKEditor</a>.</p>',
+		predefinedTextEscaped = '&lt;p&gt;This is some &lt;strong&gt;sample text&lt;/strong&gt;. You are using &lt;a href="http://ckeditor.com/"&gt;CKEditor&lt;/a&gt;.&lt;/p&gt';
 
-	function createSandbox( code ) {
+	var samplesDescriptions = {
+		'1': 'has data-sample-short attribute',
+		'2': 'has not extra attributes',
+		'3': 'has data-sample-short attribute in nested textarea',
+		'4': 'has textarea content in one line',
+		'5': 'has &quot; characters'
+	};
+
+	function createSandbox( code, forDownload ) {
+		// Default false.
+		forDownload = ( forDownload === true ? true : false );
+
 		var iframe = document.createElement( 'iframe' );
 		document.body.appendChild( iframe );
 
-		iframe.contentDocument.body.innerHTML = code.replace( /&lt;/g, '<' ).replace( /&gt;/g, '>' );
+		if ( !forDownload ) {
+			code = code.replace( /&lt;/g, '<' ).replace( /&gt;/g, '>' );
+		}
+
+		iframe.contentDocument.body.innerHTML = code;
 
 		return {
 			iframe: iframe,
@@ -16,53 +33,53 @@
 	describe( 'Creating sample source code with no double escaping characters in textarea', function() {
 		var doubleEscapeTextarea = false;
 
-		it( 'which has data-sample-short attribute', function() {
+		it( 'which ' + samplesDescriptions[ 1 ], function() {
 			var downloadSampleCode = simpleSample.createSampleSourceCode( '1', false, false, doubleEscapeTextarea ),
 				sandbox = createSandbox( downloadSampleCode );
 
-			expect( sandbox.textarea.value.trim() ).to.equal( '<p>This is some <strong>sample text</strong>. You are using <a href="http://ckeditor.com/">CKEditor</a>.</p>' );
+			expect( sandbox.textarea.value.trim() ).to.equal( predefinedText );
 		} );
 
-		it( 'which has not extra attributes', function() {
+		it( 'which ' + samplesDescriptions[ 2 ], function() {
 			var downloadSampleCode = simpleSample.createSampleSourceCode( '2', false, false, doubleEscapeTextarea ),
 				sandbox = createSandbox( downloadSampleCode );
 
 			expect( sandbox.textarea.value.trim() ).to.equal( '<p>Hello moto</p>' );
 		} );
 
-		it( 'which has data-sample-short attribute in nested textarea', function() {
+		it( 'which ' + samplesDescriptions[ 3 ], function() {
 			var downloadSampleCode = simpleSample.createSampleSourceCode( '3', false, false, doubleEscapeTextarea ),
 				sandbox = createSandbox( downloadSampleCode );
 
-			expect( sandbox.textarea.value.trim() ).to.equal( '<p>This is some <strong>sample text</strong>. You are using <a href="http://ckeditor.com/">CKEditor</a>.</p>' );
+			expect( sandbox.textarea.value.trim() ).to.equal( predefinedText );
 		} );
 	} );
 
 	describe( 'Creating sample source code with double escaping characters in textarea', function() {
 		var doubleEscapeTextarea = true;
 
-		it( 'which has data-sample-short attribute', function() {
+		it( 'which ' + samplesDescriptions[ 1 ], function() {
 			var downloadSampleCode = simpleSample.createSampleSourceCode( '1', false, false, doubleEscapeTextarea ),
 				sandbox = createSandbox( downloadSampleCode );
 
 			expect( sandbox.textarea.value.trim() ).to.equal( '&lt;p&gt;This is some &lt;strong&gt;sample text&lt;/strong&gt;. You are using &lt;a href="http://ckeditor.com/"&gt;CKEditor&lt;/a&gt;.&lt;/p&gt;' );
 		} );
 
-		it( 'which has not extra attributes', function() {
+		it( 'which ' + samplesDescriptions[ 2 ], function() {
 			var downloadSampleCode = simpleSample.createSampleSourceCode( '2', false, false, doubleEscapeTextarea ),
 				sandbox = createSandbox( downloadSampleCode );
 
 			expect( sandbox.textarea.value.trim() ).to.equal( '&lt;p&gt;Hello moto&lt;/p&gt;' );
 		} );
 
-		it( 'which has data-sample-short attribute in nested textarea', function() {
+		it( 'which ' + samplesDescriptions[ 3 ], function() {
 			var downloadSampleCode = simpleSample.createSampleSourceCode( '3', false, false, doubleEscapeTextarea ),
 				sandbox = createSandbox( downloadSampleCode );
 
 			expect( sandbox.textarea.value.trim() ).to.equal( '&lt;p&gt;This is some &lt;strong&gt;sample text&lt;/strong&gt;. You are using &lt;a href="http://ckeditor.com/"&gt;CKEditor&lt;/a&gt;.&lt;/p&gt;' );
 		} );
 
-		it( 'which has textarea content in one line', function() {
+		it( 'which ' + samplesDescriptions[ 4 ], function() {
 			var downloadSampleCode = simpleSample.createSampleSourceCode( '4', false, false, doubleEscapeTextarea ),
 				sandbox = createSandbox( downloadSampleCode );
 
@@ -70,13 +87,50 @@
 		} );
 	} );
 
-	describe( 'Creating sample which has &quot; characters', function() {
+	describe( 'Creating sample which ' + samplesDescriptions[ 5 ], function() {
 		it( 'one', function() {
-			debugger;
 			var downloadSampleCode = simpleSample.createSampleSourceCode( '5', false, false ),
 				sandbox = createSandbox( downloadSampleCode );
 
 			expect( sandbox.body.querySelector('div input').value ).to.equal( 'Execute the &quot;bold&quot; Command' );
+		} );
+	} );
+
+	describe( 'Creating sample source code for download', function() {
+		it( 'which ' + samplesDescriptions[ 1 ], function() {
+			var sampleSourceCode = simpleSample.getSampleSourceCode( '1' ),
+				sandbox = createSandbox( sampleSourceCode.download, true );
+
+			expect( sandbox.textarea.value.trim() ).to.equal( predefinedTextEscaped );
+		} );
+
+		it( 'which ' + samplesDescriptions[ 2 ], function() {
+			var sampleSourceCode = simpleSample.getSampleSourceCode( '2' ),
+				sandbox = createSandbox( sampleSourceCode.download, true );
+
+			expect( sandbox.textarea.value.trim() ).to.equal( '&lt;p&gt;Hello moto&lt;/p&gt;' );
+		} );
+
+		it( 'which ' + samplesDescriptions[ 3 ], function() {
+			var sampleSourceCode = simpleSample.getSampleSourceCode( '3' ),
+			sandbox = createSandbox( sampleSourceCode.download, true );
+
+			expect( sandbox.textarea.value.trim() ).to.equal( predefinedTextEscaped );
+		} );
+
+		it( 'which ' + samplesDescriptions[ 4 ], function() {
+			var sampleSourceCode = simpleSample.getSampleSourceCode( '4' ),
+			sandbox = createSandbox( sampleSourceCode.download, true );
+
+			expect( sandbox.textarea.value.trim() ).to.equal( '&lt;p&gt;Hello moto&lt;/p&gt;' );
+		} );
+
+		it( 'which ' + samplesDescriptions[ 5 ], function() {
+			//debugger;
+			var sampleSourceCode = simpleSample.getSampleSourceCode( '5' ),
+				sandbox = createSandbox( sampleSourceCode.download, true );
+
+			expect( sandbox.body.querySelector('input').value ).to.equal( 'Execute the "bold" Command' );
 		} );
 	} );
 
