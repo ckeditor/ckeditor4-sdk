f153aed00b9d96da0cdb111d76792111a3902d9a
Piotrek Koszuliński <pkoszulinski@gmail.com>
2015-06-28 13:59:59 +0200

    Update to 4.5.0.

--- samples/assets/simplesample.js
+++ samples/assets/simplesample.js
@@ -199,7 +199,7 @@
 				}
 			}
 
-			headResources.unshift( '<script src="http://cdn.ckeditor.com/4.4.7/standard-all/ckeditor.js"></script>' );
+			headResources.unshift( '<script src="http://cdn.ckeditor.com/4.5.0/standard-all/ckeditor.js"></script>' );
 			headResources = headResources.join( '' );
 
 			function getTemplatePre( headResources, title ) {
